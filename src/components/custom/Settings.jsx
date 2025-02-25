c706f14092ae817171aa906c6a195220562110e8
Aditya <aditya724842@gmail.com>
2025-02-25 10:09:17 +0530

    setting page done frontend

--- src/components/custom/Settings.jsx
+++ src/components/custom/Settings.jsx
@@ -17,7 +17,7 @@ export default function Settings() {
       {/* Changes password */}
       <div>
         <h1 className="text-2xl font-semibold ">Change Password</h1>
-        <form action="" className="grid gap-4 w-[80vw] sm:w-[30vw] mt-2 ">
+        <form action="" className="grid gap-4 w-[80vw] sm:w-[30vw]  ">
           <Input type="text" placeholder="Enter Previous Password" name="previousPassword"/>
           <Input type="text" placeholder="Enter New Password" name="newPassword" />
           <Button className="hover:bg-red-700">Change Password</Button>
